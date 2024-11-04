f2010b6742f6118a6c64e30538ddd667bfefc460
Jan Mesarč <jame_581@windowslive.com>
2024-11-04 23:31:26 +0100

    Player - switch animation fix

--- project/scripts/player.gd
+++ project/scripts/player.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY = -400.0
 @onready var sprite = $Sprite2D
 @onready var animation_player = $AnimationPlayer
 
+var jump_pressed : bool = false
 #func _on_ready() -> void:
 	#animation_player.play("idle")
 
@@ -15,27 +16,35 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
+	else:
+		jump_pressed = false
+
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		animation_player.play("jump")
 		velocity.y = JUMP_VELOCITY
+		jump_pressed = true
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
-		animation_player.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if is_on_floor():
-			animation_player.play("idle")
-	
+
+	select_animation(direction)
 	flip_sprite_by_direction()
 	move_and_slide()
 
+func select_animation(direction) -> void:
+	if jump_pressed:
+		animation_player.play("jump")
+	elif direction:
+		animation_player.play("run")
+	else:
+		animation_player.play("idle")
 
-func flip_sprite_by_direction():
+
+func flip_sprite_by_direction() -> void:
 	if velocity.x != 0:
 		if velocity.x < 0:
 			sprite.scale.x = -1
