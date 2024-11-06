93b05a330bda24b11570a711a03a9c9e80a26699
Jan Mesarč <jame_581@windowslive.com>
2024-11-06 16:42:17 +0100

    Add debug menu with level selection and start functionality

--- project/scripts/ui/main_menu.gd
+++ project/scripts/ui/main_menu.gd
@@ -1,10 +1,25 @@
 extends MarginContainer
 
+# Paths to the levels
+@export_category("Paths to levels")
+@export var level_paths: Array[String] = [
+	"res://maps/level_prototype.tscn",
+	"res://maps/mask_test.tscn",
+	"res://maps/test_scene.tscn",
+]
+
 @onready var version_label: Label = get_node("HBoxContainer/VBoxMenu/VersionLabel")
+
+var level_selected = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	version_label.text = "Version: " + ProjectSettings.get_setting("application/config/version")
+	
+	if level_paths.size() > 0:
+		level_selected = level_paths[0]
+	else:
+		push_error("No levels found in the level_paths array")
 
 
 func _on_start_game_button_pressed() -> void:
@@ -13,3 +28,15 @@ func _on_start_game_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_item_list_item_selected(index: int) -> void:
+	# Select the level based on the index
+	if index < level_paths.size():
+		level_selected = level_paths[index]
+	else:
+		push_error("Index out of bounds, index: " + str(index) + " level_paths.size(): " + str(level_paths.size()))
+		level_selected = level_paths[0]
+
+func _on_start_selected_level_button_pressed() -> void:
+		get_tree().change_scene_to_file(level_selected)
