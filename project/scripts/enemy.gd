f3b45c77dcca3982743714314e55ef40e7baee3c
Jan Mesarč <jame_581@windowslive.com>
2024-11-09 22:12:37 +0100

    Refactor enemy behavior to include return to start position functionality and update collision shape

--- project/scripts/enemy.gd
+++ project/scripts/enemy.gd
@@ -4,40 +4,52 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+const OFFSET_FROM_POSITION = 10
+
 @export_group("Enemy Properties")
 @export var movement_speed: float = 100.0
 @export var health: int = 100
 @export var damage: int = 10
 
+
 @onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
 @onready var sprite: Sprite2D = get_node("Sprite")
+@onready var return_timer: Timer = get_node("ReturnToStartPositionTimer")
 
 var chase_player: bool = false
 var player: Node = null
+var last_player_position: Vector2 = Vector2.ZERO
+var start_position: Vector2 = Vector2.ZERO
+var return_to_home : bool = false
+
+func _ready() -> void:
+	start_position = global_position
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	var direction = 0
 	
+	# Move the enemy.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	elif chase_player and player != null:
+	elif chase_player and player != null: # Chase the player.
 		direction = (player.global_position - global_position).normalized()
 		velocity.x = direction.x * movement_speed
-	else:
+	elif last_player_position != Vector2.ZERO: # Move towards the last player position.
+		direction = (last_player_position - global_position).normalized()
+		velocity.x = direction.x * movement_speed
+		if global_position.distance_to(last_player_position) < 5:
+			last_player_position = Vector2.ZERO
+			return_timer.start()
+	elif return_to_home: # Return to the start position.
+		direction = (start_position - global_position).normalized()
+		velocity.x = direction.x * movement_speed
+		if global_position.distance_to(start_position) < OFFSET_FROM_POSITION:
+			return_to_home = false
+	else: 
 		velocity.x = move_toward(velocity.x, 0, movement_speed)
 
-	# Handle jump.
-	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	# 	velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	# var direction := Input.get_axis("ui_left", "ui_right")
-	# if direction:
-	# 	velocity.x = direction * SPEED
-	# else:
-	# 	velocity.x = move_toward(velocity.x, 0, SPEED)
 	select_animation(direction)
 	flip_sprite_by_direction()
 	move_and_slide()
@@ -59,6 +71,11 @@ func flip_sprite_by_direction() -> void:
 			sprite.scale.x = 1
 			sprite.offset.x = 0
 
+func _on_return_to_start_position_timer_timeout() -> void:
+	if global_position.distance_to(start_position) > OFFSET_FROM_POSITION:
+		return_to_home = true
+		pass
+
 func _on_player_detect_area_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
 		print("Player entered!")
@@ -68,5 +85,6 @@ func _on_player_detect_area_body_entered(body: Node) -> void:
 func _on_player_detect_area_body_exited(body: Node) -> void:
 	if body.is_in_group("player"):
 		chase_player = false
-		player = null
+		last_player_position = player.global_position
+		player = null		
 		print("Player exited!")
