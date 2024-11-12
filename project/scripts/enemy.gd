8904b7a1967ea229daa2a7dfaebe6eb68a8de7fc
Jan Mesarč <jame_581@windowslive.com>
2024-11-12 22:57:40 +0100

    Add player position update timer and enhance enemy navigation behavior

--- project/scripts/enemy.gd
+++ project/scripts/enemy.gd
@@ -2,20 +2,22 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
 const OFFSET_FROM_POSITION = 50
 
 @export_group("Enemy Properties")
 @export var movement_speed: float = 100.0
+@export var jump_speed: float = -600.0
 @export var health: int = 100
 @export var damage: int = 25
 @export var attack_range: int = 50
+@export var player_position_changed_threshold: float = 20.0
 
 @onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
 @onready var sprite: Sprite2D = get_node("Sprite")
 @onready var return_timer: Timer = get_node("ReturnToStartPositionTimer")
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
+@onready var update_player_position_timer: Timer = $UpdatePlayerPositionTimer
 
 
 var chase_player: bool = false
@@ -24,22 +26,27 @@ var last_player_position: Vector2 = Vector2.ZERO
 var start_position: Vector2 = Vector2.ZERO
 var return_to_home: bool = false
 var is_attacking_player: bool = false
+var jump_now: bool = false
 
 func _ready() -> void:
 	start_position = global_position
 
 	# These values need to be adjusted for the actor's speed
 	# and the navigation layout.
-	navigation_agent.path_desired_distance = 3.0
-	navigation_agent.target_desired_distance = 3.0
+	navigation_agent.path_desired_distance = 20.0
+	navigation_agent.target_desired_distance = 10.0
 
 	# Make sure to not await during _ready.
 	actor_setup.call_deferred()
-	navigation_agent.link_reached.connect(_on_link_reached)
+
+	# update_player_position_timer.start()
+	update_player_position_timer.timeout.connect(update_player_position)
+
 
 func actor_setup():
 	# Wait for the first physics frame so the NavigationServer can sync.
 	await get_tree().physics_frame
+	navigation_agent.link_reached.connect(_on_link_reached)
 
 	# Now that the navigation map is no longer empty, set the movement target.
 	# set_movement_target(global_position + Vector2(-100, 0))
@@ -48,14 +55,20 @@ func set_movement_target(movement_target: Vector2):
 	navigation_agent.target_position = movement_target
 
 func _process(delta: float) -> void:
+
 	if chase_player and player != null:
 		if global_position.distance_to(player.global_position) < attack_range:
 			attack_player()
 		else:
 			is_attacking_player = false
-		set_movement_target(player.global_position)
-	elif last_player_position != Vector2.ZERO:
-		set_movement_target(last_player_position)
+		
+		# print("Player position: ", player.global_position, "Last player position: ", last_player_position)
+		# if player.global_position.distance_to(last_player_position) > player_position_offset:
+		# 	last_player_position = player.global_position
+		# 	set_movement_target(last_player_position)
+
+	# elif last_player_position != Vector2.ZERO:
+	# 	set_movement_target(last_player_position)
 	# elif return_to_home:
 	# 	set_movement_target(start_position)
 
@@ -69,26 +82,35 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 		return
 
+	if jump_now:
+		print("Jumping madafaka")
+		velocity.y = jump_speed
+		select_animation(velocity)
+		flip_sprite_by_velocity()
+		jump_now = false
+		move_and_slide()
+		return
+
 	if navigation_agent.is_navigation_finished():
 		velocity = Vector2.ZERO
 		select_animation(velocity)
 		flip_sprite_by_velocity()
 		return
 
-	if is_attacking_player:
-		velocity = Vector2.ZERO
-		select_animation(velocity)
-		flip_sprite_by_velocity()
-		return
+	# if is_attacking_player:
+	# 	velocity = Vector2.ZERO
+	# 	select_animation(velocity)
+	# 	flip_sprite_by_velocity()
+	# 	return
 
 	var current_agent_position: Vector2 = global_position
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
 
-	print("is target reached: ", navigation_agent.is_target_reached())
-	print("get_final_position: ", navigation_agent.get_final_position())
+	# print("is target reached: ", navigation_agent.is_target_reached())
+	# print("get_final_position: ", navigation_agent.get_final_position())
 	
 	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
-	print("current_agent_position: ", current_agent_position, "next_path_position: ", next_path_position, "velocity: ", velocity)
+	# print("current_agent_position: ", current_agent_position, "next_path_position: ", next_path_position, "velocity: ", velocity)
 	
 	select_animation(velocity)
 	flip_sprite_by_velocity()
@@ -117,13 +139,13 @@ func flip_sprite_by_velocity() -> void:
 			sprite.scale.x = 1
 
 
-func _on_return_to_start_position_timer_timeout() -> void:
-	last_player_position = Vector2.ZERO
-	chase_player = false
-	return_timer.stop()
+# func _on_return_to_start_position_timer_timeout() -> void:
+# 	last_player_position = Vector2.ZERO
+# 	chase_player = false
+# 	return_timer.stop()
 
-	if global_position.distance_to(start_position) > OFFSET_FROM_POSITION:
-		return_to_home = true
+# 	if global_position.distance_to(start_position) > OFFSET_FROM_POSITION:
+# 		return_to_home = true
 
 
 func _on_player_detect_area_body_entered(body: Node) -> void:
@@ -131,6 +153,9 @@ func _on_player_detect_area_body_entered(body: Node) -> void:
 		print("Player entered!")
 		chase_player = true
 		player = body
+		last_player_position = player.global_position
+		set_movement_target(last_player_position)
+		update_player_position_timer.start()
 
 
 func _on_player_detect_area_body_exited(body: Node) -> void:
@@ -139,17 +164,31 @@ func _on_player_detect_area_body_exited(body: Node) -> void:
 		last_player_position = player.global_position
 		player = null
 		# return_timer.start()
+		update_player_position_timer.stop()
 		print("Player exited!")
+
 
 func _on_attack_area_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
 		body.call_deferred("deal_damage", damage)
 		print("Player attacked!")
 
-func _on_link_reached() -> void:
-	var current_agent_position: Vector2 = global_position
-	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
-	print("Using navlink to jump from ", current_agent_position, " to ", next_path_position)
-	# Add your jump logic here, e.g., setting a jump velocity
-	velocity.y = JUMP_VELOCITY  # Example jump speed
-	# Optionally, you can add an animation or effect here
+
+func _on_link_reached(details) -> void:
+	print("Link reached details: ", details)
+	
+	var link_exit_position: Vector2 = details["link_exit_position"]
+	print("Link exit position: ", link_exit_position)
+	print("Global position: ", global_position)
+
+	if global_position.y > link_exit_position.y:
+		print("Jumping madafaka")
+		jump_now = true
+
+
+func update_player_position() -> void:
+	if player != null:
+		if player.global_position.distance_to(last_player_position) > player_position_changed_threshold:
+			last_player_position = player.global_position
+			set_movement_target(last_player_position)
+			print("Updating player position: ", last_player_position)
