d5578e8259ab2527fe8b9eed881ecf68e31bb22c
Jan Mesarč <jame_581@windowslive.com>
2024-11-12 23:00:41 +0100

    Remove unused return timer and clean up enemy movement logic

--- project/scripts/enemy.gd
+++ project/scripts/enemy.gd
@@ -15,7 +15,6 @@ const OFFSET_FROM_POSITION = 50
 
 @onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
 @onready var sprite: Sprite2D = get_node("Sprite")
-@onready var return_timer: Timer = get_node("ReturnToStartPositionTimer")
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 @onready var update_player_position_timer: Timer = $UpdatePlayerPositionTimer
 
@@ -48,29 +47,16 @@ func actor_setup():
 	await get_tree().physics_frame
 	navigation_agent.link_reached.connect(_on_link_reached)
 
-	# Now that the navigation map is no longer empty, set the movement target.
-	# set_movement_target(global_position + Vector2(-100, 0))
 
 func set_movement_target(movement_target: Vector2):
 	navigation_agent.target_position = movement_target
 
 func _process(delta: float) -> void:
-
 	if chase_player and player != null:
 		if global_position.distance_to(player.global_position) < attack_range:
 			attack_player()
 		else:
 			is_attacking_player = false
-		
-		# print("Player position: ", player.global_position, "Last player position: ", last_player_position)
-		# if player.global_position.distance_to(last_player_position) > player_position_offset:
-		# 	last_player_position = player.global_position
-		# 	set_movement_target(last_player_position)
-
-	# elif last_player_position != Vector2.ZERO:
-	# 	set_movement_target(last_player_position)
-	# elif return_to_home:
-	# 	set_movement_target(start_position)
 
 
 func _physics_process(delta: float) -> void:
@@ -97,17 +83,14 @@ func _physics_process(delta: float) -> void:
 		flip_sprite_by_velocity()
 		return
 
-	# if is_attacking_player:
-	# 	velocity = Vector2.ZERO
-	# 	select_animation(velocity)
-	# 	flip_sprite_by_velocity()
-	# 	return
+	if is_attacking_player:
+		velocity = Vector2.ZERO
+		select_animation(velocity)
+		flip_sprite_by_velocity()
+		return
 
 	var current_agent_position: Vector2 = global_position
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
-
-	# print("is target reached: ", navigation_agent.is_target_reached())
-	# print("get_final_position: ", navigation_agent.get_final_position())
 	
 	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
 	# print("current_agent_position: ", current_agent_position, "next_path_position: ", next_path_position, "velocity: ", velocity)
@@ -137,15 +120,6 @@ func flip_sprite_by_velocity() -> void:
 			sprite.scale.x = -1
 		else:
 			sprite.scale.x = 1
-
-
-# func _on_return_to_start_position_timer_timeout() -> void:
-# 	last_player_position = Vector2.ZERO
-# 	chase_player = false
-# 	return_timer.stop()
-
-# 	if global_position.distance_to(start_position) > OFFSET_FROM_POSITION:
-# 		return_to_home = true
 
 
 func _on_player_detect_area_body_entered(body: Node) -> void:
