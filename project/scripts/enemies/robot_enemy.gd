e37fcc1ef5bc87b084973ecf4aa566b974ce406f
Jan Mesarč <jame_581@windowslive.com>
2024-11-14 13:56:29 +0100

    Add RobotEnemy character with AI behavior and animations

---
+++ project/scripts/enemies/robot_enemy.gd
@@ -0,0 +1,35 @@
+extends Enemy
+
+
+func _ready() -> void:
+	super._ready()
+
+
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
+
+
+func _on_player_detect_area_body_entered(body: Node) -> void:
+	if body.is_in_group("player"):
+		print("Player entered!")
+		chase_player = true
+		player = body
+		last_player_position = player.global_position
+		set_movement_target(last_player_position)
+		update_player_position_timer.start()
+
+
+func _on_player_detect_area_body_exited(body: Node) -> void:
+	if body.is_in_group("player"):
+		chase_player = false
+		last_player_position = player.global_position
+		player = null
+		# return_timer.start()
+		update_player_position_timer.stop()
+		print("Player exited!")
+
+
+func _on_attack_area_body_entered(body: Node) -> void:
+	if body.is_in_group("player"):
+		body.call_deferred("deal_damage", damage)
+		print("Player attacked!")
