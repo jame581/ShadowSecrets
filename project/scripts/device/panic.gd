8513b62c90836563f762ac9501a151907eb78d88
Jakub Hubacek <hubacek.jakub@gmail.com>
2024-11-14 22:02:20 +0100

    Added autoplay for PANIC

--- project/scripts/device/panic.gd
+++ project/scripts/device/panic.gd
@@ -3,13 +3,19 @@ class_name PANIC
 
 #Paranoid Android Network, Inefficient and Clumsy
 
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	# Create a timer
+	var timer = Timer.new()
+	timer.wait_time = 10.0
+	timer.one_shot = true
+	timer.connect("timeout", Callable(self, "_on_Timer_timeout"))
+	add_child(timer)
+	timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_Timer_timeout():
+	$AnimationPlayer.play("enable")
