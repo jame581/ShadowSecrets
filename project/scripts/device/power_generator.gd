8fe4b71e4c08c9b855fcad515e06d9b9d72153af
Jakub Hubacek <hubacek.jakub@gmail.com>
2024-11-15 14:20:33 +0100

    Added power generator

--- project/scripts/device/power_generator.gd
+++ project/scripts/device/power_generator.gd
@@ -1,12 +1,25 @@
 extends InteractableDevice
 class_name PowerGenerator
 
+@onready var animation: AnimationPlayer = $AnimationPlayer
+
+var phase : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _interact():
+	phase += 1
+	if phase > 3:
+		animation.play("active")
+	else:
+		animation.play("sequence"+str(phase))
+
+
