34fe62454decee749043c61a846aa172031d6cce
Jakub Hubacek <hubacek.jakub@gmail.com>
2024-11-16 13:10:41 +0100

    Power generator waits for activation by 3 buttons, then activates

--- project/scripts/device/power_generator.gd
+++ project/scripts/device/power_generator.gd
@@ -1,6 +1,8 @@
 extends InteractableDevice
 class_name PowerGenerator
 
+@export var interactable_devices: Array[InteractableDevice] = []
+
 @onready var animation: AnimationPlayer = $AnimationPlayer
 
 var phase : int = 0
@@ -14,6 +16,10 @@ func _ready():
 func _process(delta):
 	pass
 
+# Triggers the outputs of all connected interactable devices.
+func trigger_outputs():
+	for interactable_device in interactable_devices:
+		interactable_device._interact()
 
 func _interact():
 	phase += 1
@@ -22,4 +28,13 @@ func _interact():
 	else:
 		animation.play("sequence"+str(phase))
 
+# In case of power generator this is called from animation player
+func set_state(state: bool):
+	enabled = state
+	if enabled:
+		trigger_outputs()
 
+func delayed_start():
+	if not enabled:
+		await get_tree().create_timer(4.0).timeout
+		set_state(true)
