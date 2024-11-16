9a71ba5e25302c63629af974545281f8e0b71005
Jakub Hubacek <hubacek.jakub@gmail.com>
2024-11-16 13:11:26 +0100

    Input simplified, can be only one-way activator

--- project/scripts/input.gd
+++ project/scripts/input.gd
@@ -1,31 +1,39 @@
 extends InteractableDevice
 class_name InputDevice
 
-@export var enabledColor: Color = Color("#a8ca58")
-@export var disabledColor: Color = Color("#cf573c")
-
-
 @export var interactable_devices: Array[InteractableDevice] = []
-
+@export var single_interaction: bool = false
 
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var collision: CollisionShape2D = $InteractionArea/CollisionShape2D
 
-func _process(_delta):
-	pass
-
-func update_state():
+# Called when the node is added to the scene.
+func _ready():
 	if sprite.material is ShaderMaterial:
-		var shader_material = sprite.material
-		var color = enabledColor if enabled else disabledColor
-		
-		shader_material.set_shader_parameter("tint_color", color)
-		shader_material.set_shader_parameter("enabled", 1.0)
+		sprite.material = sprite.material.duplicate()
+	if enabled:
+		set_state(true)
 
+# Sets the state of the device and updates its visual state.
+func set_state(state: bool):
+	enabled = state
+	update_visual_state()
+	if enabled:
+		trigger_outputs()
+
+# Called when the device is interacted with.
 func _on_interacted():
-	enabled = !enabled
-	update_state()
-	triggerOutputs()
+	set_state(not enabled)
+	if single_interaction:
+		collision.disabled = true
 
-func triggerOutputs():
+# Triggers the outputs of all connected interactable devices.
+func trigger_outputs():
 	for interactable_device in interactable_devices:
 		interactable_device._interact()
+
+# Updates the visual state of the device based on its enabled state.
+func update_visual_state():
+	if sprite.material is ShaderMaterial:
+		var shader_material: ShaderMaterial = sprite.material
+		shader_material.set_shader_parameter("enabled", 1.0 if enabled else 0.0)
