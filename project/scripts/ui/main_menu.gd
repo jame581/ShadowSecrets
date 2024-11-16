271bb01e16360880baf42dc5847e2b98e744daf4
Jan Mesarč <jame_581@windowslive.com>
2024-11-16 21:09:06 +0100

    Add input action list creation to main menu

--- project/scripts/ui/main_menu.gd
+++ project/scripts/ui/main_menu.gd
@@ -13,9 +13,18 @@ extends PanelContainer
 @onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
 @onready var settings_box: VBoxContainer = get_node("MarginContainer/HBoxContainer/VBoxSettingsMenu")
 @onready var credits_box: VBoxContainer = get_node("MarginContainer/HBoxContainer/VBoxCredits")
+@onready var controls_setting: GridContainer = get_node("MarginContainer/HBoxContainer/VBoxSettingsMenu/ControlSettingsGrid")
 
 var level_selected = ""
 var hide_credits = false
+
+var input_actions = {
+	"left": " Move Left",
+	"right": "Move Right",
+	"jump": "Jump",
+	"interact": "Interact",
+	"pause_game": "Pause Game"
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -28,6 +37,7 @@ func _ready() -> void:
 		push_error("No levels found in the level_paths array")
 	
 	animation_player.play("logo_blood")
+	create_action_list()
 
 
 func _on_start_game_button_pressed() -> void:
@@ -83,3 +93,32 @@ func _on_sounds_enabled_check_button_toggled(button_pressed: bool) -> void:
 func hide_all_settings() -> void:
 	settings_box.hide()
 	credits_box.hide()
+
+func create_action_list() -> void:
+	InputMap.load_from_project_settings()
+	clear_action_list()
+
+	for action in input_actions:
+		var action_label: Label = Label.new()
+		action_label.text = input_actions[action]
+		action_label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		
+		var input_label: Label = Label.new()
+		input_label.size_flags_horizontal = Control.SIZE_SHRINK_END & Control.SIZE_EXPAND_FILL
+		
+		
+		var events = InputMap.action_get_events(action)
+		if events.size() > 0:
+			for event in events:
+				input_label.text += event.as_text().trim_suffix(" (Physical)") + " / "
+			input_label.text = input_label.text.trim_suffix(" / ")	
+		else:
+			input_label.text = "Not set"
+		
+		controls_setting.add_child(action_label)
+		controls_setting.add_child(input_label)
+
+
+func clear_action_list() -> void:
+	for item in controls_setting.get_children():
+		item.queue_free()
