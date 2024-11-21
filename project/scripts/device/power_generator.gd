c1c3c31b78ddef86c12202d5b0e3225b79b6268f
Jakub Hubacek <hubacek.jakub@gmail.com>
2024-11-21 15:08:59 +0100

    Hooked up startup sound for power generator

--- project/scripts/device/power_generator.gd
+++ project/scripts/device/power_generator.gd
@@ -4,16 +4,12 @@ class_name PowerGenerator
 @export var interactable_devices: Array[InteractableDevice] = []
 
 @onready var animation: AnimationPlayer = $AnimationPlayer
-@onready var audio: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 var phase : int = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
@@ -37,5 +33,5 @@ func set_state(state: bool):
 
 func delayed_start():
 	if not enabled:
-		await get_tree().create_timer(2.0).timeout
+		await get_tree().create_timer(1.0).timeout
 		set_state(true)
