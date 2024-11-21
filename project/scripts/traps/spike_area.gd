26fafbbe45dd130b760ec7053c0a57e3715bcc9a
Jakub Hubacek <hubacek.jakub@gmail.com>
2024-11-21 22:55:05 +0100

    Hooked sound for spikes

--- project/scripts/traps/spike_area.gd
+++ project/scripts/traps/spike_area.gd
@@ -1,15 +1,17 @@
-extends Node2D
+extends InteractableDevice
 class_name SpikeArea
 
 enum activation_direction {LEFT, RIGHT}
 
 @export_group("Spike Area")
+@export var autostart: bool = true
 @export var spikes: Array[Spike] = []
 @export var timer_duration: float = 3.0
 @export var direction : activation_direction = activation_direction.LEFT
 
 @onready var timer: Timer = $Timer
 
+# Index of the current spike
 var spike_index: int = 0
 
 # Disable autostart for all spikes before entering the tree
@@ -22,8 +24,26 @@ func _ready():
 	for spike in spikes:
 		spike.timer.wait_time = timer_duration
 
+	if autostart:
+		print("autostart")
+		enable()
+
+func _interact():
+	toggle()
+
+func enable():
 	timer.connect("timeout", Callable(self, "_on_timer_timeout"))
 	timer.start()
+
+func disable():
+	timer.stop()
+
+func toggle():
+	enabled = !enabled
+	if enabled:
+		enable()
+	else:
+		disable()
 
 # Start the timer of the next spike
 func _on_timer_timeout():
