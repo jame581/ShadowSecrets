c49c00ae5478b94c9a1999d97a44a540f50349ac
Jakub Hubacek <hubacek.jakub@gmail.com>
2024-11-25 22:17:55 +0100

    Insanity kills the player if reaches too high

--- project/scripts/autoload/insanity.gd
+++ project/scripts/autoload/insanity.gd
@@ -12,6 +12,9 @@ var insanity: float = 0.0
 # Timer to increase insanity level
 @onready var insanity_timer: Timer = Timer.new()
 
+@export var insanity_min: float = 0.0
+@export var insanity_max: float = 1.8
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
     # Set up the timer
@@ -20,6 +23,8 @@ func _ready():
     insanity_timer.one_shot = false
     add_child(insanity_timer)
     insanity_timer.connect("timeout", Callable(self, "_on_insanity_timer_timeout"))
+
+    Global.map_changed.connect(Callable(self, "_on_map_changed"))
 
 # Function to handle insanity hit
 func insanity_hit(hit_level: insanity_level) -> void:
@@ -40,9 +45,23 @@ func increase_insanity(amount: float) -> void:
         return
 
     insanity += amount
-    emit_signal("insanity_changed", insanity)
-    RenderingServer.global_shader_parameter_set("insanity", insanity)
+    on_insanity_changed(insanity)
+    
+    if insanity >= insanity_max:
+        GameManager.game_over()
+        insanity = 0.0
+
 
 # Function called when the timer times out
 func _on_insanity_timer_timeout() -> void:
     increase_insanity(-0.02)  # Increase insanity by a small amount each time
+    
+func on_insanity_changed(new_insanity: float) -> void:
+    #print("Insanity changed to: ", new_insanity)
+    emit_signal("insanity_changed", new_insanity)
+    RenderingServer.global_shader_parameter_set("insanity", new_insanity)
+
+# Function to handle map change
+func _on_map_changed(_new_map_path: String):
+    insanity = 0.0
+    on_insanity_changed(insanity)
