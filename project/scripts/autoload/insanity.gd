f646e447d64106f73126565c7fa71d2557ccbaee
Jakub Hubacek <hubacek.jakub@gmail.com>
2024-11-25 23:30:53 +0100

    Sanity is your health now
    - in higher levels is recovers faster
    - plays camera shake in high values of insanity
    - small cleanup

--- project/scripts/autoload/insanity.gd
+++ project/scripts/autoload/insanity.gd
@@ -15,6 +15,11 @@ var insanity: float = 0.0
 @export var insanity_min: float = 0.0
 @export var insanity_max: float = 1.8
 
+@onready var camera_shaker: AnimationPlayer = get_parent().get_node("Node2D/CameraShaker")
+
+func shake_camera():
+    camera_shaker.play("shake_high")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
     # Set up the timer
@@ -30,14 +35,17 @@ func _ready():
 func insanity_hit(hit_level: insanity_level) -> void:
     match hit_level:
         insanity_level.LOW:
-            print("Insanity hit: LOW")
+            #print("Insanity hit: LOW")
             increase_insanity(0.10)
         insanity_level.MEDIUM:
-            print("Insanity hit: MEDIUM")
+            #print("Insanity hit: MEDIUM")
             increase_insanity(0.20)
         insanity_level.HIGH:
-            print("Insanity hit: HIGH")
+            #print("Insanity hit: HIGH")
             increase_insanity(0.30)
+    
+    if insanity >= insanity_max - 0.3:
+        camera_shaker.play("shake_high")
 
 # Private function to increase insanity
 func increase_insanity(amount: float) -> void:
@@ -51,13 +59,17 @@ func increase_insanity(amount: float) -> void:
         GameManager.game_over()
         insanity = 0.0
 
-
 # Function called when the timer times out
 func _on_insanity_timer_timeout() -> void:
-    increase_insanity(-0.02)  # Increase insanity by a small amount each time
+    if insanity < 0.7:
+        increase_insanity(-0.007) 
+    elif insanity < 1.2:
+        increase_insanity(-0.01)
+    elif insanity < 2.0:
+        increase_insanity(-0.02)
     
 func on_insanity_changed(new_insanity: float) -> void:
-    #print("Insanity changed to: ", new_insanity)
+    print("Insanity changed to: ", new_insanity)
     emit_signal("insanity_changed", new_insanity)
     RenderingServer.global_shader_parameter_set("insanity", new_insanity)
 
