a59a1093cd1c7ddb92cb34cdc71cdda272dc0046
Jakub Hubacek <hubacek.jakub@gmail.com>
2024-11-26 20:34:52 +0100

    Added shake into game world

--- project/scripts/autoload/insanity.gd
+++ project/scripts/autoload/insanity.gd
@@ -15,11 +15,7 @@ var insanity: float = 0.0
 @export var insanity_min: float = 0.0
 @export var insanity_max: float = 1.8
 
-#@onready var camera_shaker: AnimationPlayer = get_parent().get_node("Node2D/CameraShaker")
-var camera_shaker: AnimationPlayer = null
-func shake_camera():
-	if camera_shaker:
-		camera_shaker.play("shake_high")
+@onready var camera_shaker: AnimationPlayer = get_viewport().get_camera_2d().get_node("CameraShaker")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -31,6 +27,10 @@ func _ready():
 	insanity_timer.connect("timeout", Callable(self, "_on_insanity_timer_timeout"))
 
 	Global.map_changed.connect(Callable(self, "_on_map_changed"))
+
+	#var current_scene = get_tree().current_scene
+	#if current_scene:
+	#	camera_shaker = current_scene.get_node("Camera2D/CameraShaker")
 
 # Function to handle insanity hit
 func insanity_hit(hit_level: insanity_level) -> void:
@@ -46,10 +46,14 @@ func insanity_hit(hit_level: insanity_level) -> void:
 			increase_insanity(0.30)
 	
 	if insanity >= insanity_max - 0.3:
-		camera_shaker.play("shake_high")
+		shake_camera()
 
 # Private function to increase insanity
 func increase_insanity(amount: float) -> void:
+	if GameManager.is_god_mode:
+		insanity = 0.0
+		return
+
 	if (insanity + amount) < 0.0:
 		return
 
@@ -78,3 +82,7 @@ func on_insanity_changed(new_insanity: float) -> void:
 func _on_map_changed(_new_map_path: String):
 	insanity = 0.0
 	on_insanity_changed(insanity)
+
+func shake_camera():
+	if camera_shaker != null:
+		camera_shaker.play("shake_high")
