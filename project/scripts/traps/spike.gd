ca8e1f63b5129236c40cc4d4035e7439c27fd064
Jakub Hubacek <hubacek.jakub@gmail.com>
2024-11-30 00:23:36 +0100

    Single spike is little bit slower and gives little bit more push (still not much)

--- project/scripts/traps/spike.gd
+++ project/scripts/traps/spike.gd
@@ -5,8 +5,8 @@ class_name Spike
 @export var autostart: bool = true
 @export var active: bool = false
 @export var damage: Insanity.insanity_level = Insanity.insanity_level.LOW
-@export var timer_duration: float = 1.0
-@export var impulse_strength: float = 300.0
+@export var timer_duration: float = 1.3
+@export var impulse_strength: float = 400.0
 
 @onready var collision_shape: CollisionShape2D = $Area2D/CollisionShape2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
