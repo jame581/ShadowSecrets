64e6a60ab9c5a598797fb358dfd686d531e2a0e9
Jakub Hubacek <hubacek.jakub@gmail.com>
2024-11-29 22:03:45 +0100

    Added game end trigger device - scene, shader, texture

---
+++ project/scripts/game_end_trigger.gd
@@ -0,0 +1,22 @@
+extends InteractableDevice
+class_name GameEndTrigger
+
+
+@export_enum("Bad", "Good") var EndType: int = 0
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var collision: CollisionShape2D = $InteractionArea/CollisionShape2D
+@onready var audio: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+# Called when the node is added to the scene.
+func _ready():
+	if sprite.material is ShaderMaterial:
+		sprite.material = sprite.material.duplicate()
+
+# Called when the device is interacted with.
+func _on_interacted():
+	collision.disabled = true
+	print("You triggered the game end of type: ", "Bad" if EndType == 1 else "Good")
+	#global here
+	#GameManager.TriggerGameEnd(EndType)
+		
