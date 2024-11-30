3a537cf599dbd28f8f93658f97ec303c0e4946ea
Jan Mesarč <jame_581@windowslive.com>
2024-12-01 00:08:19 +0100

    Refactored enemy behavior and added explosion animation for robot enemies; updated game world scene with new enemy positions and parameters

--- project/scripts/enemies/robot_enemy.gd
+++ project/scripts/enemies/robot_enemy.gd
@@ -4,6 +4,8 @@ extends Enemy
 @onready var death_timer: Timer = $DeathTimer
 @onready var explosion_particles: CPUParticles2D = $ExplosionParticles
 
+var exploding: bool = false
+
 func _ready() -> void:
 	super._ready()
 
@@ -11,6 +13,18 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	super._physics_process(delta)
 
+func select_animation(direction) -> void:
+	if exploding:
+		return
+	
+	if is_attacking_player:
+		animation_player.play("attack")
+	elif direction.x:
+		animation_player.play("walk")
+		audio_component.play_with_random_pitch(movement_audio, 0.8, 1.2)
+	else:
+		animation_player.play("idle")
+		audio_component.stop_audio()
 
 func _on_player_detect_area_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
@@ -26,7 +40,6 @@ func _on_player_detect_area_body_exited(body: Node) -> void:
 		chase_player = false
 		last_player_position = player.global_position
 		player = null
-		# return_timer.start()
 		update_player_position_timer.stop()
 
 
@@ -34,6 +47,8 @@ func _on_attack_area_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
 		body.call_deferred("deal_damage", damage)
 		explosion_particles.emitting = true
+		exploding = true
+		animation_player.play("explosion")
 		audio_component.play_with_random_pitch(explosion_audio, 0.8, 1.2)
 		death_timer.start()
 
